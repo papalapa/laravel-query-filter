10b001ac0d03d39fc546122673c6dea2f115929f
Dmitriy Kim <dmitriy.kim@paybox.money>
2023-12-07 17:03:45 +0600

    Allow to redeclare SQL `LIKE` to `ILIKE` and `NOT LIKE` to `NOT ILIKE` (for postgres)

--- src/ConditionResolver.php
+++ src/ConditionResolver.php
@@ -4,15 +4,19 @@ namespace Papalapa\Laravel\QueryFilter;
 
 class ConditionResolver
 {
-    public const IS_NULL = 'IS';
+    protected const SQL_IS = 'IS';
 
-    public const IS_NOT_NULL = 'IS NOT';
+    protected const SQL_IS_NOT = 'IS NOT';
+
+    protected const SQL_LIKE = 'LIKE';
+
+    protected const SQL_NOT_LIKE = 'NOT LIKE';
 
     public function __construct(
         private ?string $value,
         private string $operator = '=',
     ) {
-        $this->process($this->value);
+        $this->process();
     }
 
     public function operator(): string
@@ -20,53 +24,77 @@ class ConditionResolver
         return $this->operator;
     }
 
-    public function isNull(): bool
-    {
-        return $this->operator === self::IS_NULL;
-    }
-
-    public function isNotNull(): bool
-    {
-        return $this->operator === self::IS_NOT_NULL;
-    }
-
     public function value(): ?string
     {
         return $this->value;
     }
 
-    private function process(?string $value): void
+    public function isNull(): bool
     {
-        if (is_null($value)) {
-            $this->operator = self::IS_NULL;
-            return;
-        }
+        return $this->operator === self::SQL_IS;
+    }
 
-        if ($value === '~') {
-            $this->operator = self::IS_NOT_NULL;
-            $this->value = null;
-            return;
-        }
+    public function isNotNull(): bool
+    {
+        return $this->operator === self::SQL_IS_NOT;
+    }
 
-        if (preg_match('/^(<>|>=|!=|<=|>|=|<|!|\*|\^|\$)/', $value, $matches)) {
-            $operator = $matches[1];
-            $value = substr($value, strlen($operator));
-            if ($operator === '!') {
-                $this->operator = 'NOT LIKE';
-                $this->value = "%{$value}%";
+    protected function process(): void
+    {
+        if (is_null($this->value)) {
+            $this->handleNull();
+        } elseif (preg_match('/^(<>|>=|!=|<=|>|=|<|~|!|\*|\^|\$)/', $this->value, $matches)) {
+            $operator    = $matches[1];
+            $this->value = substr($this->value, strlen($operator));
+            if ($operator === '~') {
+                $this->handleNotNull();
             } elseif ($operator === '*') {
-                $this->operator = 'LIKE';
-                $this->value = "%{$value}%";
+                $this->handleLike();
+            } elseif ($operator === '!') {
+                $this->handleNotLike();
             } elseif ($operator === '^') {
-                $this->operator = 'LIKE';
-                $this->value = "{$value}%";
+                $this->handleStartsWith();
             } elseif ($operator === '$') {
-                $this->operator = 'LIKE';
-                $this->value = "%{$value}";
+                $this->handleEndsWith();
             } else {
                 $this->operator = $operator;
-                $this->value = $value;
             }
         }
+    }
+
+    private function handleNull(): void
+    {
+        $this->operator = self::SQL_IS;
+        $this->value    = null;
+    }
+
+    private function handleNotNull(): void
+    {
+        $this->operator = self::SQL_IS_NOT;
+        $this->value    = null;
+    }
+
+    private function handleLike(): void
+    {
+        $this->operator = self::SQL_LIKE;
+        $this->value    = "%{$this->value}%";
+    }
+
+    private function handleNotLike(): void
+    {
+        $this->operator = self::SQL_NOT_LIKE;
+        $this->value    = "%{$this->value}%";
+    }
+
+    private function handleStartsWith(): void
+    {
+        $this->operator = self::SQL_LIKE;
+        $this->value    = "{$this->value}%";
+    }
+
+    private function handleEndsWith(): void
+    {
+        $this->operator = self::SQL_LIKE;
+        $this->value    = "%{$this->value}";
     }
 }
